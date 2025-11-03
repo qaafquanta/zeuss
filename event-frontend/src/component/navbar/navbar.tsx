056a8ae183ed4403df78d606883fc625ca7d334a
qaafquanta <qaaf.quoneta@gmail.com>
2025-11-03 18:45:02 +0700

    event management 1

--- event-frontend/src/component/navbar/navbar.tsx
+++ event-frontend/src/component/navbar/navbar.tsx
@@ -32,12 +32,7 @@ export default function Navbar() {
         // if (!res.ok) throw new Error("Not logged in");
 
         const data = await res.json();
-<<<<<<< Updated upstream
         console.log(data);
-=======
-        console.log("ini useEffect navbar Auth me")
-        console.log(data)
->>>>>>> Stashed changes
         setUser(data.user);
       } catch (err) {
         setUser(null); // token tidak valid atau belum login
@@ -85,17 +80,9 @@ export default function Navbar() {
         >
           {open ? <X size={28} /> : <Menu size={28} />}
         </button>
-<<<<<<< Updated upstream
-        {user ? (
-          <div className="flex gap-5">
-            <span>{user.username}</span>
-            <button onClick={handleLogout} className="hover:bg-white/20">
-              Logout
-            </button>
-=======
         {
           user ? 
-          <div className="flex gap-5 justify-center items-center">
+          (<div className="flex gap-5 justify-center items-center">
             <span>{user.username}</span>
             <span className="bg-white/10 rounded-full text-xs justify-center items-center py-1 px-3">{user.role}</span>
             <button
@@ -103,7 +90,6 @@ export default function Navbar() {
               className="hover:bg-white/20"
             >Logout
             </button> 
->>>>>>> Stashed changes
           </div>
         ) : (
           <p></p>
